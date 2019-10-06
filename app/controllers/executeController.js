a97d79b12e5607544b51c57736aa8fa1a5795368
Leonardo Valbuena <leov.calderon@gmail.com>
2019-10-06 17:39:51 -0500

    reportes

--- app/controllers/executeController.js
+++ app/controllers/executeController.js
@@ -1,6 +1,7 @@
 'use strict'
 var express = require('express')
 var executeService = require('../services/executeService.js');
+var reportsService = require('../services/reportsService');
 
 var router = express.Router();
 
@@ -16,15 +17,35 @@ router.get('/health', function (req, res) {
 router.post('/test', function (req, res) {
     console.log('execute test: ', JSON.stringify(req.body));
 
-    executeService.executeTest(req.body, function(apps) {
+    executeService.executeTest(req.body, function (apps) {
         res.statusCode = 200;
         res.send({ status: "OK" });
-    }, function(err) {
+    }, function (err) {
         res.statusCode = 404;
         res.send(err);
-        
+
     })
     return res;
 });
+
+
+router.post('/report', function (req, res) {
+    console.log('request report: ', req.body);
+    
+    reportsService.getReport(req.body, function (result) {
+        res.statusCode = 200;
+        res.send({ report: result });
+
+    }, function (err) {
+        res.statusCode = 404;
+        res.send(err);
+
+    })
+
+});
+
+
+
+
 
 module.exports = router;
---
+++ app/services/reportsService.js
@@ -0,0 +1,42 @@
+'use strict'
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+
+var allText = 'si nada';
+
+/**
+ * 
+ */
+module.exports.getReport = function (data, success, error) {
+    console.log('getReport...');
+
+    var nameFile = data.code + '_' + data.aplication + '.txt';
+    var basePath = 'file:///Users/leonardovalbuena/Documents/workspaceUniandes/pruebasAut/Android_worker/reports/'; 
+    var file = '';
+    
+    if (data.subType === 'random') {
+        file = basePath + data.subType + '/' + nameFile;
+    } else if (data.subType === 'BDD') {
+        file = basePath + '/bdd/' + data.aplication + '/' + nameFile;
+    }
+    readTextFile(file);
+    success(allText);
+};
+
+
+
+function readTextFile(file) {
+    console.log('ruta-reporte: ', file);
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                allText = rawFile.responseText;
+                console.log(allText);
+                return allText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
